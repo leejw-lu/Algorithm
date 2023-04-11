26d0321738e49c13994ca3cc6b7230c750f54821
leejw-lu <jiwoojenny@naver.com>
2023-04-11 22:48:44 +0900

    12865_평범한 배낭

---
+++ baekjoon/동적 프로그래밍/12865_평범한 배낭.py
@@ -0,0 +1,25 @@
+#https://www.acmicpc.net/problem/12865
+#NS(n,k)= max |  NS(n-1, k-w[n]) + val(n)
+#             |  NS(n-1, k) 
+
+import sys
+read= sys.stdin.readline
+n, k= map(int, read().split())
+
+WV=[[0,0]]
+for i in range(n):
+    WV.append(list(map(int, read().split())))
+
+ns=[[0 for i in range(k+1)] for j in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1,k+1):
+        w=WV[i][0]
+        v=WV[i][1]
+
+        if j<w:
+            ns[i][j]=ns[i-1][j]
+        else:
+            ns[i][j]=max(ns[i-1][j], ns[i-1][j-w]+v)
+
+print(ns[n][k]) #이게 최대 value값.
