df7e6de2135ef3946335278101dbbd016670596b
leejw-lu <jiwoojenny@naver.com>
2023-04-22 18:14:25 +0900

    #4 11279번 최대 힙

---
+++ 자료구조/힙/11279_최대 힙.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+read=sys.stdin.readline
+
+N=int(read())
+heap=[]
+
+for _ in range(N):
+	a=int(read())
+	if a==0:
+			if heap:
+				print(heapq.heappop(heap) [1])
+			else:
+				print("0")
+	else:
+		heapq.heappush(heap,(-a,a))  #최대힙 + 원소추가
