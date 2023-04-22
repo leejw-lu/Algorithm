f2680f24bf19c3e5805e7c6b490cfca884edffa6
jiwooLee <61766218+leejw-lu@users.noreply.github.com>
2023-04-22 18:18:14 +0900

    Update 11279_최대 힙.py

--- 자료구조/힙/11279_최대 힙.py
+++ 자료구조/힙/11279_최대 힙.py
@@ -9,9 +9,9 @@ heap=[]
 for _ in range(N):
 	a=int(read())
 	if a==0:
-			if heap:
-				print(heapq.heappop(heap) [1])
-			else:
-				print("0")
+		if heap:
+			print(heapq.heappop(heap) [1])
+		else:
+			print("0")
 	else:
 		heapq.heappush(heap,(-a,a))  #최대힙 + 원소추가
