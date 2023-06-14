6cbef4ec8193b5ecd16a3289267c58e2bac0095d
leejw-lu <jiwoojenny@naver.com>
2023-06-14 22:51:24 +0900

    #8 2차원 배열 회전 (90,180,270도)

---
+++ 코드모음/2차원 배열 회전.py
@@ -0,0 +1,29 @@
+#90도 회전
+def rotate_90(list_2d):
+    n = len(list_2d) # 행 길이 계산
+    m = len(list_2d[0]) # 열 길이 계산
+    new = [[0] * n for _ in range(m)]
+    for i in range(n):
+        for j in range(m):
+            new[j][n-i-1] = list_2d[i][j]
+    return new
+
+#180도 회전
+def rotate_180(list_2d):
+    n = len(list_2d) # 행 길이 계산
+    m = len(list_2d[0]) # 열 길이 계산
+    new = [[0] * n for _ in range(m)]
+    for i in range(n):
+        for j in range(m):
+            new[m-j-1][n-i-1] = list_2d[i][j]
+    return new
+
+#270도 회전= -90도 회전
+def rotate_270(list_2d):
+    n = len(list_2d) # 행 길이 계산
+    m = len(list_2d[0]) # 열 길이 계산
+    new = [[0] * n for _ in range(m)]
+    for i in range(n):
+        for j in range(m):
+            new[m-j-1][i] = list_2d[i][j]
+    return new
