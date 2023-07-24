62a1893aa876f39fb08d1580286a89d8342f9c6c
leejw-lu <jiwoojenny@naver.com>
2023-07-24 18:55:18 +0900

    #1 11724번 연결 요소의 개수 (다른풀이)

--- 탐색/11724_연결 요소의 개수.py
+++ 탐색/11724_연결 요소의 개수.py
@@ -24,3 +24,34 @@ for i in range(1,N+1):
         dfs(i)
         count+=1
 print(count)
+
+
+
+
+#다른 풀이도 존재
+#65148KB  700ms
+import sys
+sys.setrecursionlimit(10000)
+read=sys.stdin.readline
+
+def dfs(idx):
+    visited[idx]=True
+    for next in graph[idx]:
+        if not visited[next]:
+            dfs(next)
+
+N,M=map(int,read().split())
+graph = [[] for _ in range(N+1)]
+visited=[False]*(N+1)
+
+for _ in range(M):
+    u, v = map(int, read().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+count=0
+for i in range(1,N+1):
+    if not visited[i]:
+        dfs(i)
+        count+=1
+print(count)
