b0c1902c6e79b14130ac23bb3534a67c7501f63e
leejw-lu <jiwoojenny@naver.com>
2023-07-25 14:40:01 +0900

    #3 1439번 뒤집기

---
+++ 그리디/1439_뒤집기.py
@@ -0,0 +1,32 @@
+import sys
+read=sys.stdin.readline
+
+s=read()
+count=0
+for i in range(len(s)-1):
+    if s[i]!=s[i+1]:
+        count+=1
+print((count+1)//2)
+
+
+
+######첫시도 코드
+#str=read().rstrip()
+#arr=list(str)
+#print(arr)  >> ['0', '0', '0', '1', '1', '0', '0']
+
+#count0=arr.count('0')
+#count1=arr.count('1')
+
+#result=0
+#for i in range(len(arr)):
+ #   if count0>=count1:
+  #      if arr[i]=='1':
+   #         arr[i]='0'
+    #        result+=1
+    #else:
+     #   if arr[i]=='0':
+      #      arr[i]='1'
+       #     result+=1
+#print(result)        
+
