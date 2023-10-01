2c340a235cd69b726a7324a240dd65d0ee945206
leejw-lu <jiwoojenny@naver.com>
2023-10-01 18:21:04 +0900

    #1 2178번 미로탐색

--- 탐색/2178_미로 탐색.py
+++ 탐색/2178_미로 탐색.py
@@ -1,2 +1,29 @@
-#https://www.acmicpc.net/workbook/view/1833
-#DFS, BFS 추천문제
+import sys
+from collections import deque
+read=sys.stdin.readline
+
+dirX=[-1,1,0,0]
+dirY=[0,0,-1,1]
+
+def bfs(x,y):
+    q=deque()
+    q.append((x,y))
+
+    while q:
+        x,y=q.popleft()
+        for i in range(4):
+            newX=x+dirX[i]
+            newY=y+dirY[i]
+            if 0 <= newX < N and 0 <= newY < M:
+                if graph[newX][newY]==1: #==1을 반드시 해줘야 한다.
+                    graph[newX][newY]= graph[x][y]+1
+                    q.append((newX,newY))
+
+N,M=map(int, read().split())
+graph=[]
+
+for _ in range(N):
+    graph.append(list(map(int,read().rstrip())))
+
+bfs(0,0)
+print(graph[N-1][M-1])
